8a4b1e7c8708e0df4d780e61ddc2e9ff0bb5e4f1
Kevin-byte721 <djk6174@gmail.com>
2025-05-27 11:00:25 +0800

    Added RoomPriceStrategy Inheritance A to C

--- hotel_reservation_system.cpp
+++ hotel_reservation_system.cpp
@@ -10,3 +10,17 @@ public:
     virtual ~RoomPriceStrategy() {}
 };
 
+class RoomAPriceStrategy : public RoomPriceStrategy {
+public:
+    double getPrice() const override { return 2000.0; }
+};
+
+class RoomBPriceStrategy : public RoomPriceStrategy {
+public:
+    double getPrice() const override { return 1500.0; }
+};
+
+class RoomCPriceStrategy : public RoomPriceStrategy {
+public:
+    double getPrice() const override { return 1000.0; }
+};
